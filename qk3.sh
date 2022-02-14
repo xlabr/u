c347e8c172cbd67205785974abf001720e29893e
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-14 21:44:58 +0800

    Update and rename qk.sh to qk3.sh

--- qk3.sh
+++ qk3.sh
@@ -35,7 +35,7 @@ SYSTEMDPATH="$SYSTEMDPREFIX/$NAME.service"
 #acme
 #apt install -y socat cron curl
 #curl  https://get.acme.sh | sh
-~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
+./acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
 mkdir /usr/local/etc/acme
 chown -R $USER:$USER /usr/local/etc/acme
 acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
