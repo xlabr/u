fb58558b0a0038f601df39c2e089e386ebd2059b
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-14 22:16:02 +0800

    Update and rename qk9.sh to qk10.sh

--- qk10.sh
+++ qk10.sh
@@ -33,8 +33,8 @@ CONFIGPATH="$INSTALLPREFIX/etc/$NAME/config.json"
 SYSTEMDPATH="$SYSTEMDPREFIX/$NAME.service"
 
 #acme
-cd /home/$USER
-.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
+cd /home/$USER/.acme.sh
+acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
 mkdir /usr/local/etc/acme
 chown -R $USER:$USER /usr/local/etc/acme
 acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
