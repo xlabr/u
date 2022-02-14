d9588e80abd7411c116d3522ca125bfb2d344bb3
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-14 14:07:15 +0800

    Update qk2.sh

--- qk2.sh
+++ qk2.sh
@@ -78,9 +78,9 @@ EOF
 fi
 
 
-sed -i '8s/p.*1\"/$tpwd/g' /usr/local/etc/trojan/config.json
+sed -i '8/p.*1\"/$tpwd/g' /usr/local/etc/trojan/config.json
 sed -i '9d' /usr/local/etc/trojan/config.json
-sed -i '13s/p.*o/usr/local/etc/acme/g' /usr/local/etc/trojan/config.json
+sed -i '13/p.*o/usr/local/etc/acme/g' /usr/local/etc/trojan/config.json
 #sed -i '14s/p.*o/usr\/local\/etc\/acme/g' /usr/local/etc/trojan/config.json
 
 sudo apt install -y libcap2-bin
