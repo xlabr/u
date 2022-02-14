ebd8fc4b2aa1e9b0c1f12269a6e13343fea6dc8c
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-14 09:02:04 +0800

    Update qk.sh

--- qk.sh
+++ qk.sh
@@ -77,11 +77,13 @@ EOF
     fi
 fi
 
-echo Deleting temp directory $TMPDIR...
-rm -rf "$TMPDIR"
-
 sudo wget https://raw.githubusercontent.com/xlabr/u/sh/t.server -O /usr/local/etc/trojan/config.json
 echo trojan Done!
 
 sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/xlabr/u/sh/nginx-block.sh)"
 echo nginx Done!
+
+echo Deleting temp directory $TMPDIR...
+rm -rf "$TMPDIR"
+
+echo Done！
