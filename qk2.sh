758f6df64649b7d2512188254aff6c08e9cd7302
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-14 10:38:25 +0800

    Create qk2.sh

---
+++ qk2.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+set -euo pipefail
+
+function prompt() {
+    while true; do
+        read -p "$1 [y/N] " yn
+        case $yn in
+            [Yy] ) return 0;;
+            [Nn]|"" ) return 1;;
+        esac
+    done
+}
+
+if [[ $(id -u) != 0 ]]; then
+    echo Please run this script as root.
+    exit 1
+fi
+
+if [[ $(uname -m 2> /dev/null) != x86_64 ]]; then
+    echo Please run this script on x86_64 machine.
+    exit 1
+fi
+
+read -p "enter domain trdomain ip pwd:" tdomain trdomain tip tpwd
+
+NAME=trojan
+VERSION=$(curl -fsSL https://api.github.com/repos/trojan-gfw/trojan/releases/latest | grep tag_name | sed -E 's/.*"v(.*)".*/\1/')
+TMPDIR="$(mktemp -d)"
+INSTALLPREFIX=/usr/local
+SYSTEMDPREFIX=/etc/systemd/system
+
+BINARYPATH="$INSTALLPREFIX/bin/$NAME"
+CONFIGPATH="$INSTALLPREFIX/etc/$NAME/config.json"
+SYSTEMDPATH="$SYSTEMDPREFIX/$NAME.service"
+ 
+sudo wget -P "$TMPDIR" https://github.com/xlabr/u/releases/download/$VERSION/trojan
+sudo wget -P "$TMPDIR" https://github.com/xlabr/u/releases/download/$VERSION/server.json-example
+
+echo Entering temp directory $TMPDIR...
+cd "$TMPDIR"
+
+echo Installing $NAME $VERSION to $BINARYPATH...
+install -Dm755 "$NAME" "$BINARYPATH"
+
+echo Installing $NAME server config to $CONFIGPATH...
+if ! [[ -f "$CONFIGPATH" ]] || prompt "The server config already exists in $CONFIGPATH, overwrite?"; then
+    install -Dm644 server.json-example "$CONFIGPATH"
+else
+    echo Skipping installing $NAME server config...
+fi
+
+if [[ -d "$SYSTEMDPREFIX" ]]; then
+    echo Installing $NAME systemd service to $SYSTEMDPATH...
+    if ! [[ -f "$SYSTEMDPATH" ]] || prompt "The systemd service already exists in $SYSTEMDPATH, overwrite?"; then
+        cat > "$SYSTEMDPATH" << EOF
+[Unit]
+Description=$NAME
+Documentation=https://xlabr.github.io/$NAME/config https://xlabr.github.io/t/
+After=network.target network-online.target nss-lookup.target mysql.service mariadb.service mysqld.service
+[Service]
+Type=simple
+StandardError=journal
+ExecStart="$BINARYPATH" "$CONFIGPATH"
+ExecReload=/bin/kill -HUP \$MAINPID
+LimitNOFILE=51200
+Restart=on-failure
+RestartSec=1s
+[Install]
+WantedBy=multi-user.target
+EOF
+
+        echo Reloading systemd daemon...
+        systemctl daemon-reload
+    else
+        echo Skipping installing $NAME systemd service...
+    fi
+fi
+
+sudo wget https://raw.githubusercontent.com/xlabr/u/sh/t.server -O /usr/local/etc/trojan/config.json
+sudo apt install -y libcap2-bin
+sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/trojan
+echo trojan Done!
+
+#nginx
+sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/xlabr/u/sh/nginx-block.sh)"
+sudo bash -c 'echo "
+server {
+    listen 127.0.0.1:80 default_server;
+
+    server_name $tdomain;
+
+    include /etc/nginx/conf.d/agentdeny;
+    include /etc/nginx/bots.d/blockbots.conf;
+    include /etc/nginx/bots.d/ddos.conf;
+
+    location / {
+    proxy_pass https://$trdomain;
+    }
+}
+
+server {
+    listen 127.0.0.1:80;
+
+    server_name $tip;
+
+    include /etc/nginx/conf.d/agentdeny;
+    include /etc/nginx/bots.d/blockbots.conf;
+    include /etc/nginx/bots.d/ddos.conf;
+
+    return 301 https://$tdomain\$request_uri;
+}
+
+server {
+    listen 0.0.0.0:80;
+    listen [::]:80;
+
+    server_name _;
+
+    return 444 ;
+}
+
+" > /etc/nginx/sites-available/default'
+echo nginx Done!
+
+echo Deleting temp directory $TMPDIR...
+rm -rf "$TMPDIR"
+
+echo Done！
