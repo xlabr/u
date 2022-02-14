076482f0d3579ab1f0d50f147b743ba4b374afb6
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-14 20:53:01 +0800

    Update qk.sh

--- qk.sh
+++ qk.sh
@@ -21,28 +21,34 @@ if [[ $(uname -m 2> /dev/null) != x86_64 ]]; then
     exit 1
 fi
 
-read -p "ENTER TDOMAIN RTDOMAIN TIP TPWD:" TDOMAIN RTDOMAIN TIP TPWD
-
+#varibales
+read -p "ENTER USER TDOMAIN RTDOMAIN TIP TPWD:" USER TDOMAIN RTDOMAIN TIP TPWD
 NAME=trojan
 VERSION=$(curl -fsSL https://api.github.com/repos/trojan-gfw/trojan/releases/latest | grep tag_name | sed -E 's/.*"v(.*)".*/\1/')
 TMPDIR="$(mktemp -d)"
 INSTALLPREFIX=/usr/local
 SYSTEMDPREFIX=/etc/systemd/system
-
 BINARYPATH="$INSTALLPREFIX/bin/$NAME"
 CONFIGPATH="$INSTALLPREFIX/etc/$NAME/config.json"
 SYSTEMDPATH="$SYSTEMDPREFIX/$NAME.service"
- 
+
+#acme
+acme.sh  --issue  -d $TDOMAIN --dns dns_cf    --server  letsencrypt
+mkdir /usr/local/etc/acme
+chown -R $USER:$USER /usr/local/etc/acme
+acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
+acme.sh  --upgrade  --auto-upgrade
+chmod -R 750 /usr/local/etc/acme
+
+#trojan 1
+useradd -r trojan
+adduser trojan $USER
 wget -P "$TMPDIR" https://github.com/xlabr/u/releases/download/$VERSION/trojan
 wget -P "$TMPDIR" https://github.com/xlabr/u/releases/download/$VERSION/server.json-example
 
-echo Entering temp directory $TMPDIR...
 cd "$TMPDIR"
-
-echo Installing $NAME $VERSION to $BINARYPATH...
 install -Dm755 "$NAME" "$BINARYPATH"
 
-echo Installing $NAME server config to $CONFIGPATH...
 if ! [[ -f "$CONFIGPATH" ]] || prompt "The server config already exists in $CONFIGPATH, overwrite?"; then
     install -Dm644 server.json-example "$CONFIGPATH"
 else
@@ -68,7 +74,6 @@ RestartSec=1s
 [Install]
 WantedBy=multi-user.target
 EOF
-
         echo Reloading systemd daemon...
         systemctl daemon-reload
     else
@@ -76,59 +81,45 @@ EOF
     fi
 fi
 
-#custon trojan json
+#trojan 2
 sed -i "8s/password1\",/$TPWD\"/g" /usr/local/etc/trojan/config.json
 sed -i '13s/path\/to/usr\/local\/etc\/acme/g' /usr/local/etc/trojan/config.json
 sed -i '14s/path\/to/usr\/local\/etc\/acme/g' /usr/local/etc/trojan/config.json
 sed -i '9d' /usr/local/etc/trojan/config.json
 
-#
+#trojan 3
 chown -R trojan:trojan /usr/local/etc/trojan
 apt install -y libcap2-bin
 setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/trojan
-
 echo trojan Done!
 
 #nginx setting
-
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/xlabr/u/sh/nginx-block.sh)"
-
 cat > /etc/nginx/sites-available/default <<-EOF
 server {
     listen 127.0.0.1:80 default_server;
-
     server_name $TDOMAIN;
-
     include /etc/nginx/conf.d/agentdeny;
     include /etc/nginx/bots.d/blockbots.conf;
     include /etc/nginx/bots.d/ddos.conf;
-
     location / {
     proxy_pass https://$RTDOMAIN;
     }
 }
-
 server {
     listen 127.0.0.1:80;
-
     server_name $TIP;
-
     include /etc/nginx/conf.d/agentdeny;
     include /etc/nginx/bots.d/blockbots.conf;
     include /etc/nginx/bots.d/ddos.conf;
-
     return 301 https://$TDOMAIN\$request_uri;
 }
-
 server {
     listen 0.0.0.0:80;
     listen [::]:80;
-
     server_name _;
-
     return 444 ;
 }
-
 EOF
 echo nginx Done!
 
@@ -140,5 +131,4 @@ systemctl restart nginx
 
 echo Deleting temp directory $TMPDIR...
 rm -rf "$TMPDIR"
-
 echo Done！
