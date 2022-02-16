c6d7b06f3fcf5bae84b2be823871e4320e3a236c
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-16 20:50:35 +0800

    Update and rename qk114.sh to qk115.sh

--- qk115.sh
+++ qk115.sh
@@ -64,7 +64,7 @@ chmod -R 750 /usr/local/etc/acme
 AAA
 
 #trojan 1
-echo $PWD | su - $USER -c sudo -s <<SSS
+echo $PWD | su - $USER -c sudo <<SSS
 #echo $PWD | sudo -s <<DDD
 useradd -r trojan
 adduser trojan $USER
