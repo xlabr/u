1e95ed34a99c15922cea8614bffdf95afaf433ae
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-16 16:28:25 +0800

    Create useradd.sh

---
+++ useradd.sh
@@ -0,0 +1,22 @@
+#<999
+  if [ $(id -u) -eq 0 ]; then
+	read -p "Enter username : " username
+	read -s -p "Enter password : " password
+	egrep "^$username" /etc/passwd >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$username exists!"
+		exit 1
+	else
+999 
+    password=789123
+    username=usr
+		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+		sudo useradd -m -p "$pass" "$username"
+#<999		
+[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+	fi
+else
+	echo "Only root may add a user to the system."
+	exit 2
+fi
+999
