6998b28398822bf01fbc80752e801e15a4753331
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-16 16:50:46 +0800

    Update and rename useradd4.sh to useradd5.sh

--- useradd5.sh
+++ useradd5.sh
@@ -2,4 +2,4 @@ set -euo pipefail
 password=789123
 username=usrttt
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-sudo useradd -m -p "$pass" "$username"
+useradd -m -p "$pass" "$username"
