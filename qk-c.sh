0d07e94f540033d43e619805d4b4501ef3c0a7a6
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-17 22:17:57 +0800

    Update qk-c.sh

--- qk-c.sh
+++ qk-c.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 read -p "ENTER TDOMAIN:" TDOMAIN
 acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
 acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
+systemctl restart trojan
+systemctl restart nginx
