527026d6a4df7e985f838162fb2a5ffbfe455fd0
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-17 11:06:39 +0800

    Update and rename qk1.sh to qk2.sh

--- qk2.sh
+++ qk2.sh
@@ -43,7 +43,7 @@ chmod -R 750 /usr/local/etc/acme
 AAA
 #trojan 1
 #echo $PWD | sudo -s <<DDD
-sudo -s <<DDD
+sudo <<DDD
 useradd -r trojan
 #adduser trojan $USER
 wget -P "$TMPDIR" https://github.com/xlabr/u/releases/download/$VERSION/trojan
