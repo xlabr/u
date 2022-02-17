341fb99d7dcd6ac749f5d2ed8063cfd30a4770e1
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-17 22:16:35 +0800

    Create qk-c.sh

---
+++ qk-c.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+#varibales
+read -p "ENTER TDOMAIN:" TDOMAIN
+acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
+acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
