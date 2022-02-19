1217be830fb93d091349e0288a08a248e058a1d3
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-19 17:50:38 +0800

    Update and rename qk22-di.sh to qk-di.sh

--- qk-di.sh
+++ qk-di.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 #varibales
 read -p "ENTER TDOMAIN RTDOMAIN TIP:" TDOMAIN RTDOMAIN TIP
-#~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
-#~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
+~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
+~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
 
 sudo -s <<AAA
 cat > /etc/nginx/sites-available/default <<-EOF
