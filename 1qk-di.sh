fcccba4a2b82980a97d3b38e43be57768d0ca94a
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-19 19:14:20 +0800

    Update 1qk-di.sh

--- 1qk-di.sh
+++ 1qk-di.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 #varibales
-read -p "ENTER PWD TDOMAIN RTDOMAIN TIP:" PWD TDOMAIN RTDOMAIN TIP
-#~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
-#~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
+read -p "ENTER TDOMAIN RTDOMAIN TIP:" TDOMAIN RTDOMAIN TIP
+~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
+~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
 
-echo $PWD |sudo -S -s <<AAA
+sudo -s <<AAA
 cat > /etc/nginx/sites-available/default <<-EOF
 server {
     listen 127.0.0.1:80 default_server;
