220a4bdd904c86d7c8e2890233f6f993121f7ac7
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-19 22:24:41 +0800

    Update qk-di-pwd3.sh

--- qk-di-pwd3.sh
+++ qk-di-pwd3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-read -p "ENTER PWD USER TDOMAIN RTDOMAIN TIP:" PWD USER TDOMAIN RTDOMAIN TIP
+read -p "ENTER PWD TDOMAIN RTDOMAIN TIP:" PWD TDOMAIN RTDOMAIN TIP
 
 #~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
 #~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
 
-echo $PWD |sudo -S -u $USER bash -c "$(cat <<AAA
+echo $PWD |sudo -S bash -c "$(cat <<AAA
 cat > /etc/nginx/sites-available/default <<-EOF
 server {
     listen 127.0.0.1:80 default_server;
