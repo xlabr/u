290f8848ebab2ccf8b4ffca7fc320e9110bfb37d
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-19 17:25:12 +0800

    Update and rename qk-di.sh to qk12-di.sh

--- qk12-di.sh
+++ qk12-di.sh
@@ -5,7 +5,7 @@ read -p "ENTER TDOMAIN RTDOMAIN TIP:" TDOMAIN RTDOMAIN TIP
 #~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
 #~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
 
-sudo cat > /etc/nginx/sites-available/default <<-EOF
+cat $PWD | sudo -S cat > /etc/nginx/sites-available/default <<-EOF
 server {
     listen 127.0.0.1:80 default_server;
     server_name $TDOMAIN;
