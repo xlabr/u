0dbac1ac3d7ef60129b5321484aa2e8b2034651f
xlabr <99520033+xlabr@users.noreply.github.com>
2022-02-19 17:26:41 +0800

    Update qk12-di.sh

--- qk12-di.sh
+++ qk12-di.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 #varibales
-read -p "ENTER TDOMAIN RTDOMAIN TIP:" TDOMAIN RTDOMAIN TIP
+read -p "ENTER PWD TDOMAIN RTDOMAIN TIP:" PWD TDOMAIN RTDOMAIN TIP
 #~/.acme.sh/acme.sh --issue -d $TDOMAIN --dns dns_cf --server letsencrypt
 #~/.acme.sh/acme.sh --install-cert -d $TDOMAIN --key-file /usr/local/etc/acme/private.key --fullchain-file /usr/local/etc/acme/certificate.crt
 
